4384725ae6ebe560ee55ec9e555ae99a697925dd
Qweerty114477 <ravraj11447798@gmail.com>
2026-06-29 16:44:07 +0530

    Implement Postfix to Infix conversion in C++ using stack
    - Added C++ implementation for Postfix to Infix conversion.
    - Used stack<string> to construct infix expressions.
    - Preserved operand order while processing operators.
    - Added parentheses around sub-expressions to maintain correct precedence.
    - Included sample input and output for testing.

---
+++ PostfixToInfix/Implementation.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ string postfixtoinfix(string s){
+    int i=0;
+    stack<string>st;
+    while(i<s.size()){
+        if(isalnum(s[i])){
+            st.push(string(1,s[i]));
+        }
+        else {
+            string s1=st.top();
+            st.pop();
+            string s2=st.top();
+            st.pop();
+            string ans="("+s1+s[i]+s2+")";
+            st.push(ans);
+        }
+        i++;
+    }
+    return st.top();
+ }
+int main(){
+    string postfix = "ab+c*";
+    
+    cout << "Postfix Expression: " << postfix << endl;
+    cout << "Infix Expression:   " << postfixtoinfix(postfix) << endl;
+    return 0;
+}
